215290092597b9cfc385cbad921f6f6f6dbac2e6
Aayush Kumar <aayushkumar4917@gmail.com>
2026-01-14 21:00:16 +0530

    Version 0.2: Sum of two numbers and pattern program

---
+++ Sum.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
+// This program calculates the sum of two integers and prints the result.
