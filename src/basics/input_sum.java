ef134a849c9cbe6fb6c5783ef1e91facea90ea8b
Aayush Kumar <aayushkumar4917@gmail.com>
2026-01-16 22:50:17 +0530

    Take input from user and find sum, prdocut and area

---
+++ src/basics/input_sum.java
@@ -0,0 +1,19 @@
+package basics;
+
+import java.util.*;
+
+public class input_sum {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        int product = a * b;
+
+        System.out.println(sum);
+        System.out.println(product);
+    }
+}
