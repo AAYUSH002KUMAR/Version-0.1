a0516a1b3ab2d2ff1169405b0e0dc00b91499a0c
Aayush Kumar <aayushkumar4917@gmail.com>
2026-01-18 13:32:04 +0530

    code for type promotion simple sum

---
+++ src/basics/type_promotion_sum.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.*;
+
+public class type_promotion {
+
+    public static void main(String[] argss) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char number = 'A';
+        short number1 = 50;
+
+        int sum = number + number1;
+
+        System.out.println(sum);
+    }
+}
